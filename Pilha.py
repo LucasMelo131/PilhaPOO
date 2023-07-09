91940b2c1a5b5f472e9db16b415897322d43269a
Lucas Melo de Carvalho <lucas_m@id.uff.br>
2023-07-09 15:09:43 -0300

    criação da funcao pilha_push e atualizações

--- Pilha.py
+++ Pilha.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 class Pilha:
 
     #Construtor da classe pilha com os atributos privados
@@ -11,24 +9,49 @@ class Pilha:
     #Método que verifica se a pilha está vazia
     def empty(self):
         return self.__topo == -1
-    
+
     #Insere um valor no topo da pilha (retorna True caso consiga inserir)
     def push(self,valor):
-        if (len(self.__valores) == self.__TAM_MAX):
+        if len(self.__valores) == self.__TAM_MAX:
             return "Pilha cheia, o valor " + str(valor) + " não pode ser inserido"
         self.__valores.insert(0,valor)
         self.__topo += 1
-        #print("O valor",valor,"foi inserido!")
         return True
 
     #Insere vários valores na pilha (um por um)
     def big_push(self,*valores):
         for valor in valores:
-            encheu = self.push(valor)
-            if encheu is not True:
+            nao_encheu = self.push(valor)
+            if nao_encheu is not True:
                 print("O valor",valor,"não pode ser inserido e nem os posteriores")
                 break
-    
+
+    #Empilhar uma pilha em outra (criamos uma cópia da pilha de origem
+    #para respeitar o princípio de imutabilidade :) )
+
+    def pilha_push(self,origem):
+        #cria a copia de uma pilha p/ outra variavel
+        def __copia(pilha):
+            new_pilha = Pilha()
+            temp = []
+            while not pilha.empty():
+                elem = pilha.pop()
+                temp.append(elem)
+            for obj in temp:
+                new_pilha.push(obj)
+            del temp
+            return new_pilha
+        aux = __copia(origem)
+        #empilha na pilha de destino
+        while not aux.empty():
+            curr = aux.pop()
+            nao_encheu = self.push(curr)
+            if nao_encheu is not True:
+                print("O valor",curr,"não pode ser inserido e nem os posteriores")
+                break
+
+
+
     #Remove o elemento do topo da pilha, se ela não estiver vazia
     def pop(self):
         if self.empty():
@@ -36,7 +59,7 @@ class Pilha:
         self.__topo -= 1
         removido = self.__valores.pop(0)
         return removido
-    
+
     #Remove n elementos da pilha (especificado como parâmetro da função)
     #Se n > tam da pilha, esvazia a pilha
     def big_pop(self, quant):
@@ -46,10 +69,7 @@ class Pilha:
         self.__topo -= quant
         if self.__topo < -1:
             self.__topo = -1
-    
+
     #Retorna o topo da pilha
     def top(self):
         return self.__valores[0]
-    
-    def imprimir(self):
-        print(self.__valores)
--- Principal.py
+++ Principal.py
@@ -4,12 +4,14 @@ class Principal:
   
     def __init__(self):
         self.__pilha1 = Pilha()
-        #self.__pilha2 = Pilha()
+        self.__pilha2 = Pilha()
 
     def main(self):
         #self.__pilha1 = Pilha()
-        self.__pilha1.big_push(1,2,3,5,7,8,9,10,11,19,20)
-        self.__pilha1.imprimir()
+        self.__pilha2.big_push(1,2,3,5)
+        self.__pilha1.pilha_push(self.__pilha2)
+        #self.__pilha1.imprimir()
+        #self.__pilha2.imprimir()
 
 teste = Principal()
 teste.main()
